1c330a903d0495973d56ed65f0030ee1fa61bd9b
sharmabhushan1994@gmail.com <sharmabhushan1994@gmail.com>
2021-01-16 14:20:57 +0530

    bug fixed of Image Title Click

--- src/main/java/ImageHoster/model/User.java
+++ src/main/java/ImageHoster/model/User.java
@@ -19,7 +19,7 @@ public class User {
     @Column(name = "id")
     private Integer id;
 
-    @Column(name = "username")
+    @Column(name = "user_name")
     private String username;
 
     @Column(name = "password")
